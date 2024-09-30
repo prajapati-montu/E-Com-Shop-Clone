2a5e98351f31522eb73ab148a22b9078173a8bdf
prajapati-montu <137889257+prajapati-montu@users.noreply.github.com>
2024-09-30 15:10:45 +0530

    html

---
+++ JS/project17.js
@@ -0,0 +1,43 @@
+var x = 1;
+function middle() {
+    if (x == 2) {
+        x = 1;
+    }
+    if (x == 1) {
+        document.getElementById("testimonial-text1").style.transition="transform .3s ease";
+        document.getElementById("testimonial-text1").style.transform="leftx(0)";
+        document.getElementById("testimonial-text1").innerHTML="Till the one day when the lady met this fellow and they knew it was much more than a hunch. These Happy Days are yours and mine Happy Days. Its mission to explore strange new worlds to seek out new man has gone before.";
+        // document.getElementById("testimonial-img1").src="men1.png";
+        // document.getElementById("customer-text").innerHTML="Micheal Clerk";
+        // document.getElementById("customer-text1").innerHTML="Customer Sydney";
+        // document.getElementById("test-slider").style.transition=".2s";
+        // document.getElementById("test-slider").style.overflowX="scroll";
+
+    }
+    x++;
+}
+// function right(){
+//     if (x==2){
+//         x=1;
+//     }
+//     if(x==1){
+//         document.getElementById("testimonial-text1").innerHTML = "Till the one day when the lady met this fellow and they knew it was much more than a hunch. These Happy Days are yours and mine Happy Days. Its mission to explore strange new worlds to seek out new man has gone before.";
+//         document.getElementById("testimonial-img1").src="men2.png";
+//         document.getElementById("customer-text").innerHTML="Micheal Clerk";
+//         document.getElementById("customer-text1").innerHTML="Customer Sydney";
+//         document.getElementById("test-slider").style.transition="2s";
+//     }
+//     x++;
+// }
+// function left(){
+//     if (x==2){
+//         x=1;
+//     }
+//     if(x==1){
+//         document.getElementById("testimonial-text1").innerHTML = "Till the one day when the lady met this fellow and they knew it was much more than a hunch. These Happy Days are yours and mine Happy Days. Its mission to explore strange new worlds to seek out new man has gone before.";
+//         document.getElementById("testimonial-img1").src="customer.png";
+//         document.getElementById("customer-text").innerHTML="Micheal Clerk";
+//         document.getElementById("customer-text1").innerHTML="Customer Sydney";
+//     }
+//     x++;
+// }
